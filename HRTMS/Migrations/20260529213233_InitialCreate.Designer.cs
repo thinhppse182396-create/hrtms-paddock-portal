099e647b6acfe004022a28ff247834b0ebc5aa54
﻿Tlerd <nguyentranducanh220605@gmail.com>
2026-05-30 12:45:14 +0700

    Auto: update

--- HRTMS/Migrations/20260529213233_InitialCreate.Designer.cs
+++ HRTMS/Migrations/20260529213233_InitialCreate.Designer.cs
@@ -20,7 +20,7 @@ namespace HRTMS.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "10.0.8")
+                .HasAnnotation("ProductVersion", "8.0.8")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
